fa4584365d5162c57df90ef1fb914598ba539f8b
kur <kur@niboch-cluster.(none)>
2012-10-30 05:12:51 -0400

    fastq 2 fasta/qual translator

---
+++ fastq2fa.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+open my $fastq, "<", "$ARGV[0]" or die "no file! give fasta file in ARGV\n";
+my $filename = $ARGV[0];
+my $csfasta_filename = $filename.".csfasta";
+my $qual_name = $filename.".qual";
+open my $csfasta, ">", "$csfasta_filename";
+open my $qual, ">","$qual_name";
+
+while (1){
+    &print_one_read($fastq);
+    last if eof ($fastq)
+}
+
+sub print_one_read { my $fastq = $_[0];
+    $name = <$fastq>;
+    chomp $name;
+    $name =~ s/@/>/;
+    $read = <$fastq>;
+    print $csfasta $name, "\n", $read;
+
+    my $qualname = <$fastq>;
+    $qual_string = <$fastq>;
+    chomp $qual_string;
+    @quals = split "", $qual_string;
+    print $qual $name,"\n";
+    foreach (@quals){
+        #my $current_qual = log (1 + 10 ** ((ord ($_) - 64) / 10)) / log (10);
+        my $value = ord ($_)-33; $value-- if $value ==0;
+        printf $qual "%1.0f ", $value;
+    }
+    print $qual "\n";
+}
+
